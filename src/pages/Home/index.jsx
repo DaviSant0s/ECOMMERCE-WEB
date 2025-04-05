39b0ff96a86b73d3badbe7500882e185777015bd
Davi Santos <daviir17@gmail.com>
2025-04-04 21:29:17 -0300

    banner black friday canva

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,11 +1,11 @@
 import './styles.css';
-import banner from '../../assets/banners/banner2.png'
+import banner from '../../assets/banners/banner_blackfriday.png'
 
 const Home = () => {
     return (
         <div className='home-container'>
             <div className='banner-container'>
-                <img src={banner} alt="" />
+               <img src={banner} alt="" />
             </div>
         </div>
     );
